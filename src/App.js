6597345bc281aa9dc37da5987aeed7a89a21d856
Taron1980 <taron.jaghatspanyan@mail.ru>
2023-02-05 22:31:13 +0400

    added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import App1 from './firstFolder/firstFile';
+// import "./App1"
+
+function App() {
+  return (
+    // <h1>AAA</h1>
+ <App1 />
+  )
+  
+}
+
+export default App
---
+++ src/firstFolder/firstFile.jsx
@@ -0,0 +1,8 @@
+function App1() {
+    return (
+      <h1>AAA </h1>
+    )
+    
+  }
+  export default App1
+
